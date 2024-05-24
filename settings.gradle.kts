544caa10873738fb3b08eeb4cf5c9af0a3c6c7aa
Md. Mahamud Mredha <mahamud15-13756@diu.edu.bd>
2024-05-24 23:14:55 +0600

    BISMILLAH

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ALL_University_Repository"
+include(":app")
+ 
